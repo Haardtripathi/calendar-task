8ae7254eb5879df36d096f7e8c6d7522e2ecd0f5
unknown <haard.tripathi14@gmail.com>
2024-12-16 02:56:34 +0530

    stupipd

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "https://calendar-task-gauth.onrender.com"
+    baseURL: "https://calendar-task-demo1.onrender.com"
     // baseURL: 'http://localhost:3000', // Backend URL
     // baseURL: "https://calendar-task-6bj8.onrender.com"
 });
